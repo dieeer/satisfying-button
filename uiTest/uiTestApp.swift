b987b090ab7e4e081b04f1c31117a198fb423f09
justin reid <hi@justinre.id>
2022-10-04 15:57:56 +0100

    Initial Commit

---
+++ uiTest/uiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiTestApp.swift
+//  uiTest
+//
+//  Created by justin on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct uiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
